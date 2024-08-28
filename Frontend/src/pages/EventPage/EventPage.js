fb7cb9c159c536616311be5a46ac2bc7dc64f723
Mondo3771 <kabelomrn@gmail.com>
2024-08-28 10:58:12 +0200

    SCRUM-27 Pulling from Nathans Branch To get his cide

--- Frontend/src/pages/EventPage/EventPage.js
+++ Frontend/src/pages/EventPage/EventPage.js
@@ -1,13 +1,17 @@
-import React from 'react'
-
+import React from 'react';
+import eventImage from "../../images/ds.jpeg";
 
 const EventPage = () => {
-  return (
-  <div>
-    <h1>Event Page</h1>
-    
-  </div>
-  )
-}
 
-export default EventPage
+
+  
+  return (
+    <div>
+      <img src={eventImage} alt="Event Image" />
+      <h1>Event Page</h1>
+
+    </div>
+  );
+};
+
+export default EventPage;
