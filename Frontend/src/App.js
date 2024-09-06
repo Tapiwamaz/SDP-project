03e5aef3157131642e396407dab78063c8ff5721
Tapiwa <mazaruratapiwa03@gmail.com>
2024-09-06 19:59:44 +0200

    SCRUM-33 Reconfigured minimun passing code

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div data-testid="title" className="App">
-       <h1> We're currnetly working on a few things :(</h1>
+       <h1>We're currently working on a few things :(</h1>
     </div>
   );
 }
