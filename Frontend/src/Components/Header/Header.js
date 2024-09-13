7800a1e28bd1902ae6b3f6d0996497df33d613f1
teekayseboni <tholwana@icloud.com>
2024-09-13 17:04:47 +0200

    SCRUM-24: Changed Profile to be landscape for desktop and responsiveness

--- Frontend/src/Components/Header/Header.js
+++ Frontend/src/Components/Header/Header.js
@@ -1,108 +1,117 @@
-import React from 'react'
-import { useState,useEffect } from 'react';
-import { HeaderContainer,Xicon,Profile,ProfileIcon,Burger,Aside,AsideNavItem,Logo } from './Header.styles';
-import logo from '../../Images/Logo.svg.svg'
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  HeaderContainer,
+  Xicon,
+  Profile,
+  ProfileIcon,
+  Burger,
+  Aside,
+  AsideNavItem,
+  Logo,
+  ProfileDropdown,
+} from "./Header.styles";
+import logo from "../../Images/Logo.svg.svg";
 
-import { auth } from '../../firebase_config';
-import { useNavigate } from 'react-router';
+import { auth } from "../../firebase_config";
+import { useNavigate } from "react-router";
 
-import ProfilePage from '../Profile/ProfilePage'
+import ProfilePage from "../Profile/ProfilePage";
 
 const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const[screen,setScreen]=useState(null);
-    const [profileCllicked,setProfileClicked]=useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [screen, setScreen] = useState(null);
+  const [profileCllicked, setProfileClicked] = useState(false);
 
-    useEffect(() => {
-      const updateSlidePercentage = () => {
-        const screenWidth = window.innerWidth; // need to adjust the slide percentage based on screen size
-         if (screenWidth <= 768) {
-          // setSlidePercentage(70); // Closer to full width on small screens
-          setScreen("phone");
-        }
-       
-
-         else {
-          // setSlidePercentage(20); // Default for larger screens
-          setScreen("desktop");
-        }
-      };
-  
-      window.addEventListener('resize', updateSlidePercentage);
-      updateSlidePercentage(); // Initial check
-  
-      return () => {
-        window.removeEventListener('resize', updateSlidePercentage);
-      };
-
-
-      
-    }, []);
-
-
-
-    const storedUserData = localStorage.getItem("userData");//
-    const userData = JSON.parse(storedUserData);
-    const navigate=useNavigate();
-
-    const openProfile=()=>{
-      if(screen==='desktop'){
-        setProfileClicked(!profileCllicked)
-
-
+  useEffect(() => {
+    const updateSlidePercentage = () => {
+      const screenWidth = window.innerWidth; // need to adjust the slide percentage based on screen size
+      if (screenWidth <= 768) {
+        // setSlidePercentage(70); // Closer to full width on small screens
+        setScreen("phone");
+      } else {
+        // setSlidePercentage(20); // Default for larger screens
+        setScreen("desktop");
       }
-      else{
-        navigate('/profile');
-
-
-      }
-
-    }
-
-
-
-  
-    const toggleMenu = () => {
-      setIsOpen(!isOpen);
     };
-  
-    return (
-      <>
+
+    window.addEventListener("resize", updateSlidePercentage);
+    updateSlidePercentage(); // Initial check
+
+    return () => {
+      window.removeEventListener("resize", updateSlidePercentage);
+    };
+  }, []);
+
+  const storedUserData = localStorage.getItem("userData"); //
+  const userData = JSON.parse(storedUserData);
+  const navigate = useNavigate();
+
+  const openProfile = () => {
+    if (screen === "desktop") {
+      setProfileClicked(!profileCllicked);
+    } else {
+      navigate("/profile");
+    }
+  };
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
       <HeaderContainer>
-        
-        <Logo src={logo}/>
+        <Logo src={logo} />
 
         <Burger onClick={toggleMenu} data-testid="burger">
           <span></span>
           <span></span>
           <span></span>
         </Burger>
-        {(auth?.currentUser && storedUserData)?   
+        {auth?.currentUser && storedUserData ? (
           <Profile>
-            <img src={`${userData.imageURL}`} style={{height:"40px"}} alt='profileImg' onClick={openProfile} data-testid="profileImg"/>
+            <img
+              src={`${userData.imageURL}`}
+              style={{ height: "40px" }}
+              alt="profileImg"
+              onClick={openProfile}
+              data-testid="profileImg"
+            />
             <p>Hello {userData.name}</p>
+          </Profile>
+        ) : (
+          <Profile>
+            <ProfileIcon></ProfileIcon>
+            <p>Hello User</p>
+          </Profile>
+        )}
 
-           </Profile>
-            :
-            <Profile>
-              <ProfileIcon></ProfileIcon>
-              <p>Hello User</p>
-
-            </Profile>
-        
-        }
-        
         <Aside open={isOpen}>
-            <Xicon onClick={toggleMenu} data-testid="close-icon"></Xicon>
-          <AsideNavItem href="#home" onClick={toggleMenu}>Home</AsideNavItem>
-          <AsideNavItem href="#about" onClick={toggleMenu}>About</AsideNavItem>
-          <AsideNavItem href="#services" onClick={toggleMenu}>Services</AsideNavItem>
-          <AsideNavItem href="#contact" onClick={toggleMenu}>Contact</AsideNavItem>
+          <Xicon onClick={toggleMenu} data-testid="close-icon"></Xicon>
+          <AsideNavItem href="#home" onClick={toggleMenu}>
+            Home
+          </AsideNavItem>
+          <AsideNavItem href="#about" onClick={toggleMenu}>
+            About
+          </AsideNavItem>
+          <AsideNavItem href="#services" onClick={toggleMenu}>
+            Services
+          </AsideNavItem>
+          <AsideNavItem href="#contact" onClick={toggleMenu}>
+            Contact
+          </AsideNavItem>
         </Aside>
-
       </HeaderContainer>
-      {profileCllicked? <ProfilePage/>:null}</>
-    );
-  };
+      {/* {profileCllicked? <ProfilePage/>:null} */}
+      {/* Profile Dropdown */}
+      {screen === "desktop" && (
+        <ProfileDropdown isOpen={profileCllicked}>
+          <ProfilePage />
+        </ProfileDropdown>
+      )}
+    </>
+  );
+};
 
-export default Header
+export default Header;
--- Frontend/src/Components/Profile/ProfilePage.js
+++ Frontend/src/Components/Profile/ProfilePage.js
@@ -13,6 +13,7 @@ import {
   ButtonWrapper,
   LeftSection,
   ButtonGrp,
+  Numbers,
 } from "./ProfilePage.styles";
 import profileimg from "../../Images/profileimg.jpg";
 import {
@@ -25,12 +26,20 @@ import Header from "../Header/Header";
 
 const profile = () => {
   return (
-  
-   
     <Wrapper>
       <Summary>
         <ProfileImage src={profileimg} />
+      </Summary>
+      <Numbers>
         <h2>Emily Smith</h2>
+
+        <Rating>
+          <StarIcon
+            style={{ height: "3vh", width: "3vw", color: "black" }}
+          ></StarIcon>{" "}
+          {/* Star Icon */}
+          <h4 style={{ color: "#676363" }}>4.0 rating from 48 users</h4>
+        </Rating>
         <Count>
           <EventsGrp>
             {" "}
@@ -44,28 +53,18 @@ const profile = () => {
             <h3>Events Attended</h3>
           </EventsGrp>
         </Count>
-        <Rating>
-          <StarIcon
-            style={{ height: "3vh", width: "3vw", color: "black" }}
-          ></StarIcon>{" "}
-          {/* Star Icon */}
-          <h4 style={{ color: "#676363" }}>4.0 rating from 48 users</h4>
-        </Rating>
-      </Summary>
-
+      </Numbers>
       <Details>
         <Email>
           <h4>Email</h4>
           <h4 style={{ color: "#676363" }}>emily.smith@eventsgrp.co.za</h4>
         </Email>
-
         <About>
           <h4>About</h4>
           <h4 style={{ color: "#676363" }}>
             I'm an organized and passionate event planner who loves turning
             ideas into unforgettable experiences. With a keen eye for detail and
-            a talent for bringing people together, I use this app to create
-            seamless, personalized events that leave a lasting impression.
+            a talent for bringing people together.
           </h4>
         </About>
       </Details>
@@ -73,19 +72,19 @@ const profile = () => {
         <ButtonWrapper>
           <LeftSection>
             <BellIcon
-              style={{ height: "25px", width: "22px", color: "black" }}
+               style={{ height: "27px", width: " 22px", color: "black" }}
             />
 
             <Text>Notifications</Text>
           </LeftSection>
           <ArrowRightIcon
-            style={{ height: "25px", width: " 22px", color: "black" }}
+            style={{ height: "27px", width: " 22px", color: "black" }}
           />
         </ButtonWrapper>
         <ButtonWrapper>
           <LeftSection>
             <ArrowRightStartOnRectangleIcon
-              style={{ height: "25px", width: "22px", color: "black" }}
+              style={{ height: "27px", width: "22px", color: "black" }}
               isLogout={true}
             >
               {/* <LogoutIcon /> */}
@@ -94,13 +93,11 @@ const profile = () => {
             <Text isLogout={true}>Log Out</Text>
           </LeftSection>
           <ArrowRightIcon
-            style={{ height: "25px", width: "22px", color: "black" }}
+            style={{ height: "27px", width: " 22px", color: "black" }}
           />
         </ButtonWrapper>
       </ButtonGrp>
     </Wrapper>
-
-    
   );
 };
 
