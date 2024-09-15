6a16380dd3996a4627030850b0cae2787a888296
Sam-243 <mbuyukas@gmail.com>
2024-09-15 21:48:36 +0200

    SCRUM-36: pulled from Admin

---
+++ Frontend/src/Components/ViewEvents/MyEvents/MyEvents.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'; // for API calls
+import './MyEvents.css'; // Add appropriate CSS styles
+import ViewCards from '../ViewCards/ViewCards'; // Assuming ViewCards is a separate component
+import { Events } from '../../Mocky/Mocky';
+import Header from '../../Header/Header';
+import AsideDesktop from '../../AsideDesktop/AsideDesktop';
+
+const MyEvents = () => {
+  // const [activeTab, setActiveTab] = useState('upcoming');
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    // Simulate fetching event data or fetch from API
+    // Replace this with an actual API request to get events for the organizer
+    setEvents(Events); // mockData is imported
+  }, []);
+
+
+  const handleCancel = async (event_id) => {
+    try {
+      await axios.delete(`/api/events/${event_id}`);
+      setEvents(events.filter(event => event.id !== event_id));
+    } catch (error) {
+      console.error('Error cancelling event:', error);
+    }
+  };
+
+  const renderViewCards = (event) => (
+    <ViewCards
+      key={event.id}
+      event={event}
+      onCancel={event.status === 'upcoming' ? handleCancel : null}
+    />
+  );
+
+  return (
+    <div>
+      <Header />
+      <AsideDesktop />
+      <div > {/*className="my-events-page"*/}
+        <h1>My Events</h1>
+
+        <div className="myevents-list">
+          {events.map(renderViewCards)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyEvents;
