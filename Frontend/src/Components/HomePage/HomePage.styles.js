25f371e86e15eba477371ba840e74ca8fe45c3c1
Mondo3771 <kabelomrn@gmail.com>
2024-10-28 10:26:23 +0200

    fixed the problem

--- Frontend/src/Components/HomePage/HomePage.styles.js
+++ Frontend/src/Components/HomePage/HomePage.styles.js
@@ -110,7 +110,7 @@ position: fixed; /* Keeps the element fixed to the right */
   box-shadow: -2px 0 10px rgba(0, 0, 0, 0.1); /* Adds a subtle shadow for depth */
   z-index: 1000; /* Ensures it appears above other elements */
   padding: 1em; /* Adds some padding inside the element */
-  width:450px;
+  width:350px;
   overflow-y: auto; /* Allows scrolling if the content overflows */
 `
 
