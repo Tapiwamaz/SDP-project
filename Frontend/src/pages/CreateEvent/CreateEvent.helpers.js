1154e2cfe53c86e9be55f83148f757e03e4d9274
Tapiwa <mazaruratapiwa03@gmail.com>
2024-11-25 15:56:51 +0200

    Remove grid on tags Validation on typed in date

--- Frontend/src/pages/CreateEvent/CreateEvent.helpers.js
+++ Frontend/src/pages/CreateEvent/CreateEvent.helpers.js
@@ -67,6 +67,16 @@ export const handleNextButtonClick = async (
     toast.warn("Please a vaild date");
     return;
   }
+  if ((new Date(eventDetailsT.date) - new Date()) < 0) {
+    eventRefsT.eventDate.current.classList.add("unfilled-input");
+    eventRefsT.eventDate.current.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+
+    toast.warn("You can not book for a past date (or today)");
+    return;
+  }
 
   if (!eventDetailsT.venue_type || eventDetailsT.venue_type == "") {
     // Handle missing event venue type
